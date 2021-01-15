28ce8fcc420ea8d2759e683eb1d3685e9f786d35
Aaron Leopold <aaronleopold1221@gmail.com>
2021-01-15 12:48:34 -0500

    updated Code component

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import React from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import {
   vscDarkPlus,
   vs,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
-import { MutuallyExclusive } from "../types";
+} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { MutuallyExclusive } from 'types';
 
 type ChildProps = {
   children: React.ReactText;
@@ -19,7 +19,8 @@ type Props = MutuallyExclusive<ChildProps, StringProps> & {
   slim?: boolean;
   // TODO: type me
   language?: string;
-  theme?: "light" | "dark";
+  theme?: 'light' | 'dark';
+  maxHeight?: string;
 };
 
 export default function Code({
@@ -27,26 +28,31 @@ export default function Code({
   children,
   rounded,
   slim,
-  language = "typescript",
-  theme = localStorage.theme ?? "dark",
+  language = 'typescript',
+  theme = localStorage.theme ?? 'dark',
+  maxHeight,
 }: Props) {
   const text = codeString ?? children;
-  const styles =
-    theme === "light"
+  let styles: any =
+    theme === 'light'
       ? {
-          borderRadius: rounded ? ".375rem" : 0,
-          backgroundColor: "#F9FAFB",
-          padding: slim ? ".25rem" : "1em",
+          borderRadius: rounded ? '.375rem' : 0,
+          backgroundColor: '#F9FAFB',
+          padding: slim ? '.25rem' : '1em',
         }
       : {
-          borderRadius: rounded ? ".375rem" : 0,
-          padding: slim ? ".25rem" : "1em",
+          borderRadius: rounded ? '.375rem' : 0,
+          padding: slim ? '.25rem' : '1em',
         };
+
+  if (maxHeight) {
+    styles.maxHeight = maxHeight;
+  }
 
   return (
     <SyntaxHighlighter
       language={language}
-      style={theme === "dark" ? vscDarkPlus : vs}
+      style={theme === 'dark' ? vscDarkPlus : vs}
       customStyle={styles}
     >
       {text}
